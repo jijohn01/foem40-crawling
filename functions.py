40ec6527f11355d9293af06f95cb88017c91a9f3
jijohn01 <jijohn01@gmail.com>
2019-03-03 22:33:30 +0900

    - scroll control - click more button - exception handling(1box not 5 contents)

--- functions.py
+++ functions.py
@@ -8,3 +8,4 @@ class function:
         self.driver.find_element_by_id("loginEmail").send_keys(kakao_id)
         self.driver.find_element_by_id("loginPw").send_keys(kakao_pw)
         self.driver.find_element_by_xpath("//*[@id='login-form']/fieldset/button").click()
+    #def scrollControl(self,five_cnt, cnt):
--- kakao2nav.py
+++ kakao2nav.py
@@ -10,6 +10,8 @@ kakao_pw = 'js**91^^11'
 author_name = '김인겸_인큐브랜드'
 #variable
 contents_cnt = 0
+scroll_y = 350
+foems = []
 
 f.go2kakao_login(kakao_id,kakao_pw)
 
@@ -17,9 +19,22 @@ driver.implicitly_wait(3)
 driver.find_element_by_xpath("//span[text()='"+author_name+"'][1]").click()
 
 driver.implicitly_wait(10)
-for contents_cnt in range(5):
-    foem = driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[4]/div[1]/div").text
-    if foem[0:3]=='[글]':
-        print('pass')
-    print(foem)
-    print(foem[0:3])
+for five_contents_idx in range (1,10):
+    cnt = driver.find_elements_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div["+str(five_contents_idx)+"]/div/div/div")
+    for contents_cnt in range(1,len(cnt)+1):
+
+        # 스크롤
+        scroll_y = driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div[" + str(five_contents_idx) + "]/div/div/div[" + str(contents_cnt) + "]").location
+        driver.execute_script("window.scrollTo(0," + str(scroll_y) + ")")
+
+        #더보기 클릭
+        if driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div["+str(five_contents_idx)+"]/div/div/div["+str(contents_cnt)+"]/div[1]/div[4]/p[@class='more _moreBtnContainer']").is_displayed():
+            driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div[" + str(five_contents_idx) + "]/div/div/div[" + str(contents_cnt) + "]/div[1]/div[4]/p[@class='more _moreBtnContainer']/a").click()
+        #text 가져오기
+        foem = driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div["+str(five_contents_idx)+"]/div/div/div["+str(contents_cnt)+"]/div[1]/div[4]/div[1]/div").text
+
+        #시 인지 판별
+        if foem[0:3]=='[시]':
+            print('#####################\n묶음 {} 번째, {}번째글\n'.format(five_contents_idx,contents_cnt))
+            foems.append(foem)
+            print(foem)
