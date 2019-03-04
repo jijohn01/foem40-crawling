5181901868743b2d2b1c45ee6450072e0bc3392d
jijohn01 <jijohn01@gmail.com>
2019-03-04 22:55:48 +0900

    functions - naver login - naver go2 blog write - tab change main - scroll modify

--- functions.py
+++ functions.py
@@ -2,10 +2,31 @@
 class function:
     def __init__(self,driver):
         self.driver = driver
-    def go2kakao_login(self,kakao_id,kakao_pw):
+    def kakao_go2_login(self,kakao_id,kakao_pw):
         self.driver.get('https://accounts.kakao.com/login/kakaostory')
 
         self.driver.find_element_by_id("loginEmail").send_keys(kakao_id)
         self.driver.find_element_by_id("loginPw").send_keys(kakao_pw)
         self.driver.find_element_by_xpath("//*[@id='login-form']/fieldset/button").click()
-    #def scrollControl(self,five_cnt, cnt):
+    def click_more(self,five_contents_idx,contents_cnt):
+        if self.driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div["+str(five_contents_idx)+"]/div/div/div["+str(contents_cnt)+"]/div[1]/div[4]/p[@class='more _moreBtnContainer']").is_displayed():
+            self.driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div[" + str(five_contents_idx) + "]/div/div/div[" + str(contents_cnt) + "]/div[1]/div[4]/p[@class='more _moreBtnContainer']/a").click()
+    def is_foem(self,foem):
+        if foem[0:3] == '[시]':
+            return True
+        return False
+    def naver_go2_login(self):
+        self.driver.get('https://naver.com')
+
+        self.driver.find_element_by_xpath('//*[@id="account"]/div/a/i').click()
+
+    def naver_go2_blog_write(self):
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        self.driver.get("https://blog.naver.com/jijohn01?Redirect=Write")
+
+
+    def naver_tap_change(self):
+        self.driver.switch_to.window(self.driver.window_handles[0])
+
+    def kakao_tap_change(self):
+        self.driver.switch_to.window(self.driver.window_handles[1])
--- kakao2nav.py
+++ kakao2nav.py
@@ -1,23 +1,36 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import functions
-
+import time
 driver = webdriver.Chrome('./chromedriver')
 f =functions.function(driver)
 
 #statistic
 kakao_id = 'xygene01@nate.com'
 kakao_pw = 'js**91^^11'
+naver_id = 'jijohn01'
+naver_pw = 'js**91^^1119'
 author_name = '김인겸_인큐브랜드'
 #variable
 contents_cnt = 0
 scroll_y = 350
 foems = []
 
-f.go2kakao_login(kakao_id,kakao_pw)
+
+#네이버 로그인 접근 후 아이디 비번은 수동으로 입력하여 로그인 해야함
+f.naver_go2_login()
+wait = WebDriverWait(driver,20)
+blog = wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@id='PM_ID_ct']/div[1]/div[2]/div[1]/ul[1]/li[3]/a")))
+blog.click()
+
+driver.execute_script("window.open('');")
+f.kakao_tap_change()
+f.kakao_go2_login(kakao_id,kakao_pw)
 
 driver.implicitly_wait(3)
 driver.find_element_by_xpath("//span[text()='"+author_name+"'][1]").click()
-
 driver.implicitly_wait(10)
 for five_contents_idx in range (1,10):
     cnt = driver.find_elements_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div["+str(five_contents_idx)+"]/div/div/div")
@@ -25,16 +38,19 @@ for five_contents_idx in range (1,10):
 
         # 스크롤
         scroll_y = driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div[" + str(five_contents_idx) + "]/div/div/div[" + str(contents_cnt) + "]").location
-        driver.execute_script("window.scrollTo(0," + str(scroll_y) + ")")
+        driver.execute_script("window.scrollTo(0," + str(scroll_y['y']) + ")")
 
-        #더보기 클릭
-        if driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div["+str(five_contents_idx)+"]/div/div/div["+str(contents_cnt)+"]/div[1]/div[4]/p[@class='more _moreBtnContainer']").is_displayed():
-            driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div[" + str(five_contents_idx) + "]/div/div/div[" + str(contents_cnt) + "]/div[1]/div[4]/p[@class='more _moreBtnContainer']/a").click()
+        driver.implicitly_wait(10)
+        f.click_more(five_contents_idx,contents_cnt)
         #text 가져오기
         foem = driver.find_element_by_xpath("//*[@id='myStoryContentWrap']/div[2]/div/div/div[1]/div["+str(five_contents_idx)+"]/div/div/div["+str(contents_cnt)+"]/div[1]/div[4]/div[1]/div").text
 
-        #시 인지 판별
-        if foem[0:3]=='[시]':
-            print('#####################\n묶음 {} 번째, {}번째글\n'.format(five_contents_idx,contents_cnt))
-            foems.append(foem)
-            print(foem)
+        #시 인지 판별 아니면 다음글로
+        if not f.is_foem(foem):
+            continue
+        f.naver_tap_change()
+        f.naver_go2_blog_write()
+        f.kakao_tap_change()
+        print('#####################\n묶음 {} 번째, {}번째글\n'.format(five_contents_idx,contents_cnt))
+        foems.append(foem)
+        print(foem)
